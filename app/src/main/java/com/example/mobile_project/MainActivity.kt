46fb518e091fce4935260dc02da3b79038580552
leejj9058 <113232473+leejj9058@users.noreply.github.com>
2023-11-07 01:02:20 +0900

    Update MainActivity.kt
    newnote 버튼 누르면 write 화면으로 전환

--- app/src/main/java/com/example/mobile_project/MainActivity.kt
+++ app/src/main/java/com/example/mobile_project/MainActivity.kt
@@ -1,11 +1,21 @@
-package com.example.mobile_project
+package com.example.team
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import com.example.team.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.newNote.setOnClickListener{
+            val intent = Intent(this, Write::class.java)
+            startActivity(intent)
+        }
     }
 }
